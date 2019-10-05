619fea82f90f9a547dcb5c574c72ab5bb54de939
Shubham Sagar <shubham.sagar7696@gmail.com>
2019-10-05 10:00:30 -0400

    added jpanel, jscroll and jtabel to show map

--- src/com/concordia/riskgame/view/MapEditor.java
+++ src/com/concordia/riskgame/view/MapEditor.java
@@ -9,12 +9,27 @@ import javax.swing.border.Border;
 import java.awt.*;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MapEditor extends JFrame {
 
-    public Map gameMap;
+    private JLabel countriesLabel;
+    private JLabel continentLabel;
     private ToolBar toolBar;
+    public Map gameMap;
     private MapEditorController mapEditorController;
+    private JTree mapTree;
+    private JScrollPane treeScrollPane;
+    private JPanel contientLabelViewPanel;
+    private JPanel countryLabelViewPanel;
+    private List<String> count = new ArrayList<String>();
+    private String data[][];
+    private String countryColumn[];
+    private JTable tableMatrix;
+    private JScrollPane scrollPane;
+
+
 
 
 
@@ -37,12 +52,44 @@ public class MapEditor extends JFrame {
         });
     }
     public void addComponents(MapEditorController mapEditorController) {
-        setLayout(null);
         Border blackline = BorderFactory.createLineBorder(Color.black);
         Dimension frameSize = this.getSize();
         toolBar = new ToolBar(mapEditorController);
         toolBar.setBounds(0, 0, frameSize.width, 40);
+        contientLabelViewPanel = new JPanel();
+        continentLabel = new JLabel("Continent Tree");
+        Dimension continentSize = continentLabel.getPreferredSize();
+        continentLabel.setFont(new Font("TimesRoman", Font.BOLD, 20));
+        continentLabel.setBounds(140, 50, continentSize.width + 500, continentSize.height);
+        contientLabelViewPanel.setBounds(10, 35, frameSize.width - 950, 35);
+        add(contientLabelViewPanel);
+        contientLabelViewPanel.setBackground(Color.lightGray);
+        contientLabelViewPanel.setLayout(new FlowLayout());
+        continentLabel.setFont(new Font("dialog", 1, 15));
+        contientLabelViewPanel.setBorder(blackline);
+        contientLabelViewPanel.add(continentLabel);
+
+        treeScrollPane = new JScrollPane(mapTree);
+        treeScrollPane.setBounds(10, 70, frameSize.width - 950, frameSize.height - 600);
+        countryLabelViewPanel = new JPanel();
+        countriesLabel = new JLabel("Countries Matrix");
+        Dimension countriesSize = countriesLabel.getPreferredSize();
+        countriesLabel.setFont(new Font("TimesRoman", Font.BOLD, 20));
+        countriesLabel.setBounds(240, 50, countriesSize.width + 500, countriesSize.height);
+        countryLabelViewPanel.setBounds(contientLabelViewPanel.getBounds().x + (int) (contientLabelViewPanel.getBounds().getWidth()), 35,frameSize.width - 300, 35);
+        add(countryLabelViewPanel);
+        countryLabelViewPanel.setBackground(Color.lightGray);
+        countryLabelViewPanel.setLayout(new FlowLayout());
+        countriesLabel.setFont(new Font("dialog", 1, 15));
+        countryLabelViewPanel.setBorder(blackline);
+        countryLabelViewPanel.add(countriesLabel);
+        scrollPane = new JScrollPane(tableMatrix);
+        scrollPane.setBounds(treeScrollPane.getBounds().x + (int) (treeScrollPane.getBounds().getWidth()), 70,frameSize.width - 300, frameSize.height - 600);
+
+        add(scrollPane);
+        add(treeScrollPane);
         add(toolBar);
+
 
     }
 
