9675fb2b74cba9b8d1c062ddc5f7e62170821a04
san089 <sanchitkumar013@gmail.com>
2019-10-16 15:26:16 -0400

    Adding test case

--- test/TestSuite.java
+++ test/TestSuite.java
@@ -1,7 +1,6 @@
 
 //JUnit 4 test suite
 import com.concordia.riskgame.model.Modules.Dice;
-import com.concordia.riskgame.tests.StartUpPhaseTests;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
@@ -10,7 +9,7 @@ import org.junit.runners.Suite.SuiteClasses;
 import com.concordia.riskgame.controller.CommandControllerTest;
 
 @RunWith(Suite.class)
-@SuiteClasses({Dice.class})
+@SuiteClasses({com.concordia.riskgame.tests.DiceTest.class})
 
 /**
  * This is the Test Suite class
---
+++ test/com/concordia/riskgame/utilities/MapToolsTest.java
@@ -0,0 +1,55 @@
+package com.concordia.riskgame.utilities;
+
+import com.concordia.riskgame.model.Modules.Map;
+import org.junit.Test;
+
+
+import static org.junit.Assert.*;
+
+public class MapToolsTest {
+
+    @Test
+    public void pickMapFileService() {
+        String currentPath = System.getProperty("user.dir");
+        String pathSuffixValidMaps = currentPath + "\\Maps\\Valid_Maps\\";
+        String pathSuffixInvalidMaps = currentPath + "\\Maps\\Invalid_Maps\\";
+
+        //=============================INVALID MAPS=============================================
+        String countryNotConnectedInItsContinent = pathSuffixInvalidMaps + "CountryNotConnectedInItsContinent.map";
+        String informationMissing = pathSuffixInvalidMaps + "InformationMissing.map";
+        String mapWithNoCountryTest = pathSuffixInvalidMaps + "MapWithNoCountryTest.map";
+        String twoContinentNotConnected = pathSuffixInvalidMaps + "TwoContinentNotConnected.map";
+
+        //=============================VALID MAPS=============================================
+        String bigValidMap = pathSuffixValidMaps + "BigValidMap.map";
+        String smallValidMap = pathSuffixValidMaps + "SmallValidMap.map";
+
+
+        MapTools mp = new MapTools();
+
+        printLine();
+        System.out.println("Test Case 1. Country is a neighbour of country in other continent but not a neighbour in its own continent");
+        assertFalse(mp.pickMapFileService(new Map(), countryNotConnectedInItsContinent));
+        printLine();
+        System.out.println("Test Case 2. A missing tag/annotation in map file.");
+        assertFalse(mp.pickMapFileService(new Map(), informationMissing));
+        printLine();
+        System.out.println("Test Case 3. A map that has a continent with no country");
+        assertFalse(mp.pickMapFileService(new Map(), mapWithNoCountryTest));
+        printLine();
+        System.out.println("Test Case 4. A map having two continent but the continents are not connected.");
+        assertFalse(mp.pickMapFileService(new Map(), twoContinentNotConnected));
+
+        printLine();
+        System.out.println("Test Case 5. A Small valid map.");
+        assertTrue(mp.pickMapFileService(new Map(), smallValidMap));
+        printLine();
+        System.out.println("Test Case 5. A Big valid map.");
+        assertTrue(mp.pickMapFileService(new Map(), bigValidMap));
+
+    }
+
+    public void printLine(){
+        System.out.println("========================================================================================");
+    }
+}
